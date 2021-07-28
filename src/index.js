29fce060f71ce98ce31f1d0cefc65a6cb68c6666
Artur <vashpartner@gmail.com>
2021-07-28 20:28:33 +0300

    Очистка кода

--- src/index.js
+++ src/index.js
@@ -10,24 +10,4 @@ const render = new Render('.game_body__middle')
 menu.registerComponent(render)
 render.registerComponent(menu)
 
-// a.hide()
 
-// bb.start()
-// setTimeout(bb.repeatI.bind(bb,true), 3000)
-// setTimeout(bb.repeatI.bind(bb,false), 6000)
-
-// bb.pauseGame()
-// bb.repeatI()
-// bb.pauseGame()
-// bb.startGame()
-
-// bb.restartGame()
-
-// setTimeout(bb.continueGame.bind(bb), 2000)
-
-// setTimeout(bb.pauseGame.bind(bb), 5000)
-// setTimeout(bb.continueGame.bind(bb), 8000)
-// setTimeout(bb.pauseGame.bind(bb), 10000)
-
-
-// setTimeout(bb.restartGame.bind(bb), 13000)
